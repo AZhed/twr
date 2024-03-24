d52840ca94b5688b9ede367c6a9cde8330874c24
AZhed <andrei.jedancov@gmail.com>
2024-03-24 12:45:10 +0100

    feat: add node joint_state_publisher to rviz launch file

--- twr_sim/launch/rviz.launch.py
+++ twr_sim/launch/rviz.launch.py
@@ -14,6 +14,8 @@ def generate_launch_description():
     # Create launch description
     ld = LaunchDescription()
 
+
+    #  === rviz2 ===
     twr_description_pkg_path = get_package_share_directory('twr_description')
 
     rviz_config_file = os.path.join(
@@ -34,6 +36,7 @@ def generate_launch_description():
         arguments=["-d", rviz_config_file])
 
 
+    # === robot_state_publisher ===
     urdf_config_file = Command(['xacro ', xacro_config_file])
 
     robot_state_publisher_param = [{'robot_description': urdf_config_file}]
@@ -41,11 +44,19 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=robot_state_publisher_param)
-        
+        parameters=robot_state_publisher_param
+    )
+
+    #  === joint_state_publisher ===
+    # necessary for correct representation of axes in rviz2
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher'
+    )
 
 
     ld.add_action(rviz2_node)
     ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
 
     return ld
